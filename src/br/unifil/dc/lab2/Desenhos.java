73aa5bb123b1c1954138f4e4971514d79074931d
Diogo Fernandes <diogo.fernandes@lb2.com.br>
2020-04-14 19:04:33 -0300

    First and only

---
+++ src/br/unifil/dc/lab2/Desenhos.java
@@ -0,0 +1,23 @@
+package br.unifil.dc.lab2;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+
+
+/**
+ * Write a description of class Desenhos here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Desenhos
+{
+    public static void desenhoLivre(Graphics2D pincel, Dimension dim) {
+        throw new UnsupportedOperationException("O aluno ainda não implementou este método.");
+    }
+    
+    public static void desenharAsterisco(Graphics2D g2d) {
+        throw new UnsupportedOperationException("O aluno ainda não implementou este método.");
+    }
+    
+}
